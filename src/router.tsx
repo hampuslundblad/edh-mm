2f0fd6e69de3cb62a024bb0242e7993678964249
hampuslundblad <31845093+hampuslundblad@users.noreply.github.com>
2025-06-07 17:26:26 +0200

    update base path in router

--- src/router.tsx
+++ src/router.tsx
@@ -1,21 +1,21 @@
 import { createRouter } from "@tanstack/react-router"
 
 // Import the generated route tree
-import { routeTree } from './routeTree.gen'
-
+import { routeTree } from "./routeTree.gen"
 
 // Create a new router instance
 export const router = createRouter({
+  basepath: "/edh-mm",
   routeTree,
   context: {},
-  defaultPreload: 'intent',
+  defaultPreload: "intent",
   scrollRestoration: true,
   defaultStructuralSharing: true,
   defaultPreloadStaleTime: 0,
 })
 
 // Register the router instance for type safety
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router
   }
