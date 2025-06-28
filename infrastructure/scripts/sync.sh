910113c21ad65122cf2499937d4008079718cdaa
hampuslundblad <hampus.lundblad@hotmail.se>
2025-06-28 13:54:19 +0200

    add script since im lazy

---
+++ infrastructure/scripts/sync.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Base raw URL for your GitHub repo
+BASE_URL="https://raw.githubusercontent.com/hampuslundblad/edh-mm/main/infrastructure"
+
+FILES="
+docker-compose.yaml
+nginx/default.conf
+dockerfile
+"
+
+for file in $FILES; do
+  # Skip files in scripts/
+  case "$file" in
+    scripts/*) continue ;;
+  esac
+  echo "Downloading $file..."
+  curl -o "$file" "$BASE_URL/$file"
+done
