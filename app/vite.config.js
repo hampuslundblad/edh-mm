b0a247afad3e5f8d3308f53249d78e98841e908a
hampuslundblad <hampus.lundblad@hotmail.se>
2025-06-29 12:26:56 +0200

    change ports

--- app/vite.config.js
+++ app/vite.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://localhost:5000",
         rewrite: (path) => path.replace(/^\/api/, ""),
         changeOrigin: false,
         secure: false,
